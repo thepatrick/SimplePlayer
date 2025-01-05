f0f08e1769f3e8d02dca93478138c9148c50f53e
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-01-05 11:23:49 +1100

    Initial Commit

---
+++ SimplePlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimplePlayer
+//
+//  Created by Patrick Quinn-Graham on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimplePlayer/SimplePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePlayerApp.swift
+//  SimplePlayer
+//
+//  Created by Patrick Quinn-Graham on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SimplePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
